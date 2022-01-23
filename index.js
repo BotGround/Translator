6bade32600478ec3695a0a22969f6cc76f1ebd7a
Thebigbot <77632836+Thebigbot0000@users.noreply.github.com>
2022-01-23 15:50:52 +0100

    Create /translate commands

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js')
+const translate = require('translate-google')
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
 const env = require('dotenv').config()
 
@@ -16,6 +17,7 @@ client.on('ready', () => {
                 name: 'commands',
                 description: 'Get information about commands',
                 type: 'STRING',
+                required: false,
                 choices: [
                     {
                        name: 'help',
@@ -27,12 +29,67 @@ client.on('ready', () => {
                     },
                     {
                         name: 'links',
-                        value: 'translate_cmd'
+                        value: 'links_cmd'
                     },
                 ]
             },
         ]
     })
+
+    commands?.create({
+        name: 'translate',
+        description: 'Translate text to language',
+        options: [
+            /* {
+                name: 'from',
+                description: 'Your text language code',
+                type: 'STRING',
+                required: true,
+            }, */
+            {
+                name: 'to',
+                description: 'Language to translate your text',
+                type: 'STRING',
+                required: true,
+            },
+            {
+                name: 'text',
+                description: 'Text to translate',
+                type: 'STRING',
+                required: true,
+            },
+        ]
+    })
+
+    commands?.create({
+        name: 'links',
+        description: 'Bot links',
+    })
 })
 
 client.login(process.env.TOKEN)
+
+
+client.on('interactionCreate', i => {
+    if (!i.isCommand()) return;
+
+    if (i.commandName === 'translate') {
+        let options = {
+            from: i.options.getString("from"),
+            to: i.options.getString("to"),
+            text: i.options.getString("text"),
+        }
+
+        translate(options.text, { to: options.to }).then(res => {
+            let e = new Discord.MessageEmbed()
+            .setAuthor(i.user.username, i.user.avatarURL({ dynamic: true }))
+            .setDescription("```"+res+"```")
+            .setColor("RANDOM")
+            i.reply({
+                embeds: [e]
+            })
+        }).catch(err => {
+            
+        })
+    }
+})
